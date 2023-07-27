97d4d9dd80e30a080c4883a6ccd621271ee3d46b
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-07-27 20:22:20 +0900

    create_1st

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php基礎編</title>
+</head>
+
+<body>
+  <p>この文章はHTMLで出力しています。</p>
+  <p>
+    <?php
+    echo 'この文章はPHPで出力しています。';
+    ?>
+  </p>
+</body>
+
+</html>
+
